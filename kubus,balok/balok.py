3674d05c297cab7ac084316f804ef2944d2a8d06
NuhaAfia <113221820+NuhaAfia@users.noreply.github.com>
2022-09-16 14:55:41 +0700

    program menghitung

---
+++ kubus,balok/balok.py
@@ -0,0 +1,8 @@
+#volume balok 
+print("menghitung luas dan volume balok")
+p = int(input("masukkan panjang balok = "))
+l = int(input("masukkan luas balok = "))
+t = int(input("masukkan tinggi balok = "))
+volumebalok = (2*p*l) + (2*p*t) + (2*l*t)
+
+print ("luas baloknya adalah = ", volumebalok)
---
+++ kubus,balok/kubus.py
@@ -0,0 +1,6 @@
+#volume kubus
+print("menghitung volume kubus")
+s = int(input("masukkan panjang rusuk kubus="))
+volumekubus = s * s * s
+print("volume kubusnya adalah" , volumekubus)
+
